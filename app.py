75c54fb8132630d53e94ad39ce269d3bd5cd1757
ananyajoshi2301 <ananyajoshi2318@gmail.com>
2020-12-12 16:29:39 +0530

    Changes done

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask,request
+import json
 
 app = Flask(__name__)
 
+def configure_routes(app):
 
-@app.route('/')
-def index():
-    return "Hello World"
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
 
+    @app.route('/post/test', methods=['POST'])
+    def receive_post():
+        headers = request.headers
+
+        auth_token = headers.get('authorization-sha256')
+        if not auth_token:
+            return 'Unauthorized', 401
+
+        data_string = request.get_data()
+        data = json.loads(data_string)
+
+        request_id = data.get('request_id')
+        payload = data.get('payload')
+
+        if request_id and payload:
+            return 'Ok', 200
+        else:
+            return 'Bad Request', 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
